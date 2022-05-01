584fa0731a34e962be701bdf31c32620d0281679
Anonymoushackingtool <74699731+anonymoushackingtool@users.noreply.github.com>
2022-05-01 15:25:39 +0530

    Create ytstreams.py

---
+++ pytube/ytstreams.py
@@ -0,0 +1,53 @@
+# This module is made for getting json of yt video details
+def get_streams(YouTube):
+  """
+  YouTube : Youtube object
+    from pytube.ytstreams import get_streams
+    yt = YouTube(yt_url)
+    get_streams(yt)
+  """
+    qualities = YouTube.vid_info['streamingData']
+    dash = qualities['adaptiveFormats']
+    progressive = qualities['formats']
+    streamList = []
+    for i in dash:
+        mimeType = i['mimeType']
+        itag = i['itag']
+        stream = {
+            'itag': itag,
+            'mime_type': mimeType
+        }
+
+        if 'video' in mimeType:
+            stream['fps'] = i['fps']
+            stream['resolution'] = i['qualityLabel']
+            stream['pixel_size'] = f"{i['width']}x{i['height']}"
+        try:
+            stream['file_size'] = i['contentLength']
+        except KeyError:
+            pass
+
+        streamList.append(stream)
+
+    for i in progressive:
+        mimeType = i['mimeType']
+        itag = i['itag']
+        fps = i['fps']
+        res = i['qualityLabel']
+        pixelsize = f"{i['width']}x{i['height']}"
+        stream = {
+            'itag': itag,
+            'mime_type': mimeType,
+            'fps': fps,
+            'resolution': res,
+            'pixel_size': pixelsize
+        }
+
+        try:
+            stream['file_size'] = i['contentLength']
+        except KeyError:
+            pass
+
+        streamList.append(stream)
+
+    return streamList
